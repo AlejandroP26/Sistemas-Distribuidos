48ded1febac3aa25a736836172d1d0dafc244ba7
AlejandroP26 <113127589+AlejandroP26@users.noreply.github.com>
2024-04-09 19:12:42 -0500

    resources directory

--- resources/views/dashboard.blade.php
+++
@@ -1,8 +0,0 @@
-<x-app-layout>
-    
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            @livewire('create-post')
-        </div>
-    </div>
-</x-app-layout>
